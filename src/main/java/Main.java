f31b4109cdf082a7c54f30ea9e6e33906758dc14
darsen44 <dariy.senyshyn44@gmail.com>
2018-05-02 20:50:12 +0300

    modify Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,19 +8,15 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
 
     public static void main(String[] args) {
-        long startTime;
-        long endTime;
+        long startTime, endTime;
         ForkJoinPool pool = new ForkJoinPool();
-        int[] array1 = Sort.fillArray(MyArrays.HUNDRED_THOUSAND);
         startTime = System.currentTimeMillis();
-        pool.invoke(new Sort(array1, 0, Counts.HUNDRED_THOUSAND - 1, Counts.HUNDRED_THOUSAND));
+        pool.invoke(new Sort(Sort.fillArray(MyArrays.HUNDRED_THOUSAND), 0, Counts.HUNDRED_THOUSAND - 1, Counts.HUNDRED_THOUSAND));
         endTime = System.currentTimeMillis();
         System.out.println("Time taken for sorting with FJP: " + (endTime - startTime) + " millis");
-        int[] array2 = Sort.fillArray(MyArrays.HUNDRED_THOUSAND);
         startTime = System.currentTimeMillis();
-        Sort.bubbleSort(array2, 0, Counts.HUNDRED_THOUSAND - 1);
+        Sort.bubbleSort(Sort.fillArray(MyArrays.HUNDRED_THOUSAND), 0, Counts.HUNDRED_THOUSAND - 1);
         endTime = System.currentTimeMillis();
         System.out.println("Time taken for sorting without FJP: " + (endTime - startTime) + " millis");
-
     }
 }
