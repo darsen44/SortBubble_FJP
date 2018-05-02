f6f4db1984ae326317536775ecd26cf3fa6cbdf7
darsen44 <dariy.senyshyn44@gmail.com>
2018-05-02 16:56:14 +0300

    modify compute()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,12 +13,12 @@ public class Main {
         ForkJoinPool pool = new ForkJoinPool();
         int[] array1 = Sort.fillArray(MyArrays.HUNDRED_THOUSAND);
         startTime = System.currentTimeMillis();
-        pool.invoke(new Sort(array1, 0, Counts.HUNDRED_THOUSAND - 1));
+        pool.invoke(new Sort(array1, 0, Counts.HUNDRED_THOUSAND - 1, Counts.HUNDRED_THOUSAND));
         endTime = System.currentTimeMillis();
         System.out.println("Time taken for sorting with FJP: " + (endTime - startTime) + " millis");
         int[] array2 = Sort.fillArray(MyArrays.HUNDRED_THOUSAND);
         startTime = System.currentTimeMillis();
-        Sort.bubbleSort(Sort.fillArray(MyArrays.HUNDRED_THOUSAND), 0, Counts.HUNDRED_THOUSAND);
+        Sort.bubbleSort(Sort.fillArray(MyArrays.HUNDRED_THOUSAND), 0, Counts.HUNDRED_THOUSAND - 1);
         endTime = System.currentTimeMillis();
         System.out.println("Time taken for sorting without FJP: " + (endTime - startTime) + " millis");
 
--- src/main/java/sort/Sort.java
+++ src/main/java/sort/Sort.java
@@ -7,40 +7,41 @@ public class Sort extends RecursiveAction {
     private int[] array;
     private int end;
     private int start;
+    private int count;
 
-    public Sort(int[] array, int start, int end) {
+    public Sort(int[] array, int start, int end, int count) {
         this.array = array;
         this.end = end;
         this.start = start;
+        this.count = count;
     }
 
     @Override
     protected void compute() {
-        if ((end - start) <= 50000) {
+        if ((end - start) <= count >>> 1) {
             bubbleSort(array, start, end);
-        } else{
+        } else {
             int middle = (end - start) >>> 1;
-            invokeAll(new Sort(array,start,middle),
-                      new Sort(array,middle,end));
-            merge(array,start,end);
-       }
-}
+            invokeAll(new Sort(array, start, middle, count),
+                    new Sort(array, middle, end, count));
+            merge(array, start, end);
+        }
+    }
 
-    private void merge(int[] array, int start,int end){
+    private void merge(int[] array, int start, int end) {
         int middle = (end - start) >>> 1;
         int[] ar1 = new int[middle];
         int[] ar2 = new int[middle];
-        System.arraycopy(array,start,ar1,0,middle);
-        System.arraycopy(array,middle,ar2,0,middle);
+        System.arraycopy(array, start, ar1, 0, middle);
+        System.arraycopy(array, middle, ar2, 0, middle);
 
-        int i =0, j=0, k=0;
-        while (i < middle && j < middle){
-            if(ar1[i] < ar2[j]){
+        int i = 0, j = 0, k = 0;
+        while (i < middle && j < middle) {
+            if (ar1[i] < ar2[j]) {
                 array[k] = ar1[i];
                 i++;
                 k++;
-            }
-            else{
+            } else {
                 array[k] = ar2[j];
                 j++;
                 k++;
@@ -48,7 +49,6 @@ public class Sort extends RecursiveAction {
 
         }
     }
-
 
 
     public static void bubbleSort(int[] numArray, int start, int end) {
